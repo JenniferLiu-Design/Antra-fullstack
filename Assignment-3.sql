0ea2116a77520845b735d5732d1ed114685d70cf
JenniferLiu-Design <67850601+JenniferLiu-Design@users.noreply.github.com>
2021-09-27 02:35:54 -0700

    Update Assignment-3.sql

--- Assignment-3.sql
+++ Assignment-3.sql
@@ -22,5 +22,36 @@ GROUP BY ProductID
 SELECT City, SUM(Quantity) FROM Orders o JOIN [Order details] od on o.orderid join customers c ON c.customerid=o.customerID
 GROUP BY city
 
-SELECT city FROM customers
-EXCEPT SELECT 
+select city from Customers
+except
+select city from customers
+group by city
+having COUNT(*)=1
+union 
+select city from customers
+group by city
+having COUNT(*)=0
+
+select city from customers group by city having COUNT(*)>=2
+
+select distinct city from orders o join [order details] od on o.orderid=od.orderid join customers c on c.customerid=o.CustomerID
+group by city,ProductID
+having COUNT(*)>=2
+
+select distinct c.CustomerID from orders o join [order details] od on o.orderid=od.orderid join customers c on c.customerid=o.CustomerID
+where City <> ShipCity
+
+select top 5 ProductID,AVG(UnitPrice) as AvgPrice,(select top 1 City from Customers c join Orders o on o.CustomerID=c.CustomerID join [Order Details] od2 on od2.OrderID=o.OrderID where od2.ProductID=od1.ProductID group by city order by SUM(Quantity) desc) as City from [Order Details] od1
+group by ProductID 
+order by sum(Quantity) desc
+
+select distinct City from Employees where city not in (select ShipCity from Orders where ShipCity is not null)
+
+select distinct City from Employees where City is not null except (select ShipCity from Orders where ShipCity is not null)
+
+select (select top 1 City from Orders o join [Order Details] od on o.OrderID=od.OrderID join Employees e on e.EmployeeID = o.EmployeeID
+group by e.EmployeeID,e.City
+order by COUNT(*) desc) as MostOrderedCity,
+(select top 1 City from Orders o join [Order Details] od on o.OrderID=od.OrderID join Employees e on e.EmployeeID = o.EmployeeID
+group by e.EmployeeID,e.City
+order by sum(Quantity) desc) as MostQunatitySoldCity
